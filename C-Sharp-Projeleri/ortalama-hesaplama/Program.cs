9e33cc33e4d8ecaf2cdb92aa9d359ccea7126419
Mahmut Baysağ <mbaysag1080@gmail.com>
2022-09-08 00:39:53 +0300

    upload

---
+++ C-Sharp-Projeleri/ortalama-hesaplama/Program.cs
@@ -0,0 +1,20 @@
+﻿// Kulanıcıdan alınan derinliğe göre fibonacci serisindeki rakamların ortalamasını alıp ekrana yazdıran uygulama
+
+static double fib(int s){
+    if(s==0 || s==1){ return 1;}
+    else{ return fib(s-1) + fib(s);}
+}
+
+Console.WriteLine("Derinlik Miktaırnı Giriniz: ");
+int derinlik = int.Parse(Console.ReadLine());
+
+double ort = 0;
+double top=0;
+for(int i=0;i<derinlik;i++){
+    top += fib(i);
+}
+
+ort = top / derinlik;
+Console.Write($"Ortalama={ort.ToString()}");
+
+Console.ReadKey();
